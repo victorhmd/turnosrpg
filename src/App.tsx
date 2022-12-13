cbfac57208a5c9421b399073593bcb302b73eaa7
Victor <vh.daniel96@gmail.com>
2022-12-13 18:14:49 -0300

    lista de turnos, mobile WiP

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import { Person, SmartToy, List, PlayArrow, Refresh } from '@mui/icons-material/';
-import { Button, Container } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import CharList from './components/charList/CharList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Character } from './types/Character';
+import { Turn } from './types/Turn';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -43,76 +44,191 @@ function a11yProps(index: number) {
 export default function App() {
 
   //#region States
-  const [valueTabPLayerList, setValueTabPlayerList] = useState(0);
+  const [valueTabCharacters, setValueTabCharacters] = useState(0);
+  const [valueTabTurns, setValueTabTurns] = React.useState(0);
+  const [valueTabMobile, setValueTabMobile] = React.useState(0);
+
   const [listPlayer, setListPlayer] = useState<Character[]>([]);
   const [listNpc, setListNpc] = useState<Character[]>([]);
-  const [valueTabNpcList, setValueTabNpcList] = React.useState(0);
+
+  const [charTurnList, setCharTurnList] = React.useState<Character[]>([]);
   //#endregion
 
-  //#region Variaveis
-  const handleChangeTabPlayerList = (event: React.SyntheticEvent, newValue: number) => {
-    setValueTabPlayerList(newValue)
+  //#region Variables
+  const handleChangeTabCharacters = (event: React.SyntheticEvent, newValue: number) => {
+    setValueTabCharacters(newValue)
   };
 
-  const handleChangeTabNpcList = (event: React.SyntheticEvent, newValue: number) => {
-    setValueTabNpcList(newValue);
+  const handleChangTabTurns = (event: React.SyntheticEvent, newValue: number) => {
+    setValueTabTurns(newValue);
+  };
+
+  const handleChangTabMobile = (event: React.SyntheticEvent, newValue: number) => {
+    setValueTabMobile(newValue);
   };
   //#endregion
 
   //#region Functions
   function click_AtualizarBtn() {
-    console.log('listPlayer', listPlayer);
-    console.log('listNpc', listNpc);
+    setTurnList();
   }
 
   function click_ComecarBtn() {
 
   }
+
+  function setTurnList() {
+    var list = [...listPlayer ?? [], ...listNpc ?? []];
+    list.sort((a, b) => +b.initiative - +a.initiative);
+    setCharTurnList(list);
+  }
   //#endregion
 
 
+
   return (
-    <Container sx={{ display: { xs: 'none', lg: 'flex' } }}>
-      {/* Panel - Lista de Personagens */}
-      <Box sx={{ width: '50vw !important', height: '80vh', border: 1, borderColor: 'divider', m: 3, mt: 8 }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={valueTabPLayerList} onChange={handleChangeTabPlayerList} aria-label="basic tabs example">
-            <Tab icon={<Person />} iconPosition="start" label="Jogadores" {...a11yProps(0)} />
-            <Tab icon={<SmartToy />} iconPosition="start" label="NPCs" {...a11yProps(1)} />
-          </Tabs>
+    <Container>
+      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+        {/* Panel - Lista de Personagens */}
+        <Box sx={{ width: '50vw !important', height: '80vh', border: 1, borderColor: 'divider', m: 3, mt: 8 }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={valueTabCharacters} onChange={handleChangeTabCharacters} aria-label="basic tabs example">
+              <Tab icon={<Person />} iconPosition="start" label="Jogadores" {...a11yProps(0)} />
+              <Tab icon={<SmartToy />} iconPosition="start" label="NPCs" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+
+          <TabPanel value={valueTabCharacters} index={0}>
+            <CharList inner render={1} updateCharList={(list: React.SetStateAction<Character[]>) => setListPlayer(list)} list={listPlayer}></CharList>
+          </TabPanel>
+          <TabPanel value={valueTabCharacters} index={1}>
+            <CharList render={2} updateCharList={(list: React.SetStateAction<Character[]>) => setListNpc(list)} list={listNpc}></CharList>
+          </TabPanel>
         </Box>
 
-        <TabPanel value={valueTabPLayerList} index={0}>
-          <CharList inner render={1} updateCharList={(list: React.SetStateAction<Character[]>) => setListPlayer(list)}></CharList>
-        </TabPanel>
-        <TabPanel value={valueTabPLayerList} index={1}>
-          <CharList render={2} updateCharList={(list: React.SetStateAction<Character[]>) => setListNpc(list)}></CharList>
-        </TabPanel>
+        {/* Panel - Gerenciamento de turno */}
+        <Box sx={{ width: '50vw', minHeight: '80vh', border: 1, borderColor: 'divider', m: 3, mt: 8 }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={valueTabTurns} onChange={handleChangTabTurns} aria-label="basic tabs example">
+              <Tab icon={<List />} iconPosition="start" label="Lista" {...a11yProps(0)} />
+              <Tab icon={<PlayArrow />} iconPosition="start" label="Combate" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={valueTabTurns} index={0}>
+            <Grid container spacing={1} alignItems="center" mb={3}>
+              <Grid item xs={6} textAlign="end">
+                <Button variant="contained" color="primary" endIcon={<Refresh />} onClick={click_AtualizarBtn} fullWidth>
+                  Atualizar
+                </Button>
+              </Grid>
+              <Grid item xs={6} textAlign="center">
+                <Button variant="contained" color="success" endIcon={<PlayArrow />} onClick={click_ComecarBtn} fullWidth>
+                  Começar
+                </Button>
+              </Grid>
+            </Grid>
+
+            {charTurnList.map((char, index) => (
+              <Grid container spacing={1} alignItems="center" mb={3} key={index} justifyContent="space-evenly">
+                <Grid item xs={1} textAlign="end">
+                  <Typography>{index + 1}</Typography>
+                </Grid>
+                <Grid item xs={1} textAlign="center">
+                  {char.flag === 'j' ? <Person /> : <SmartToy />}
+                </Grid>
+                <Grid item xs={5}>
+                  <Typography>{char.name}</Typography>
+                </Grid>
+                <Grid item xs={3} textAlign="center">
+                  <Typography><strong>INIT </strong>{char.initiative}</Typography>
+                </Grid>
+              </Grid>
+            ))}
+          </TabPanel>
+          <TabPanel value={valueTabTurns} index={1}>
+
+          </TabPanel>
+        </Box>
       </Box>
 
-      {/* Panel - Gerenciamento de turno */}
-      <Box sx={{ width: '50vw', minHeight: '80vh', border: 1, borderColor: 'divider', m: 3, mt: 8 }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={valueTabNpcList} onChange={handleChangeTabNpcList} aria-label="basic tabs example">
-            <Tab icon={<List />} iconPosition="start" label="Lista" {...a11yProps(0)} />
-            <Tab icon={<PlayArrow />} iconPosition="start" label="Combate" {...a11yProps(1)} />
+      <Box sx={{ display: { xs: 'flex', md: 'none', justifyContent: 'center' } }}>
+        <Box
+          sx={{ bgcolor: 'background.paper', display: 'flex', height: '85vh', mt: 3 }}
+        >
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={valueTabMobile}
+            onChange={handleChangTabMobile}
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+          >
+            <Tab icon={<Person />}{...a11yProps(0)} />
+            <Tab icon={<SmartToy />}{...a11yProps(1)} />
+            <Tab icon={<List />} {...a11yProps(2)} />
+            <Tab icon={<PlayArrow />}{...a11yProps(3)} />
           </Tabs>
-        </Box>
-        <TabPanel value={valueTabNpcList} index={0}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-            <Button variant="contained" color="primary" endIcon={<Refresh />} onClick={click_AtualizarBtn}>
-              Atualizar
-            </Button>
-            <Button variant="contained" color="success" endIcon={<PlayArrow />} onClick={click_ComecarBtn}>
-              Começar
-            </Button>
-          </Box>
-        </TabPanel>
-        <TabPanel value={valueTabNpcList} index={1}>
+          <TabPanel value={valueTabMobile} index={0} >
+            <Box sx={{ width: '60vw' }}>
+              <Typography>
+                <strong>Lista de Jogadores</strong>
+              </Typography>
+              <Box>
+                <CharList inner render={1} updateCharList={(list: React.SetStateAction<Character[]>) => setListPlayer(list)} list={listPlayer}></CharList>
+              </Box>
+            </Box>
+          </TabPanel>
+          <TabPanel value={valueTabMobile} index={1}>
+            <Box sx={{ width: '60vw' }}>
+              <Typography>
+                <strong>Lista de NPCs</strong>
+              </Typography>
+              <Box>
+                <CharList inner render={2} updateCharList={(list: React.SetStateAction<Character[]>) => setListPlayer(list)} list={listNpc}></CharList>
+              </Box>
+            </Box>
+          </TabPanel>
+          <TabPanel value={valueTabMobile} index={2}>
+            <Box sx={{ width: '60vw' }}>
+              <Grid container spacing={1} alignItems="center" mb={3}>
+                <Grid item xs={6} textAlign="end">
+                  <Button variant="contained" color="primary" endIcon={<Refresh />} onClick={click_AtualizarBtn} fullWidth>
+                    Atualizar
+                  </Button>
+                </Grid>
+                <Grid item xs={6} textAlign="center">
+                  <Button variant="contained" color="success" endIcon={<PlayArrow />} onClick={click_ComecarBtn} fullWidth>
+                    Começar
+                  </Button>
+                </Grid>
+              </Grid>
 
-        </TabPanel>
+              {charTurnList.map((char, index) => (
+                <Grid container spacing={1} alignItems="center" mb={3} key={index} justifyContent="space-evenly">
+                  <Grid item xs={1} textAlign="end">
+                    <Typography>{index + 1}</Typography>
+                  </Grid>
+                  <Grid item xs={1} textAlign="center">
+                    {char.flag === 'j' ? <Person /> : <SmartToy />}
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Typography>{char.name}</Typography>
+                  </Grid>
+                  <Grid item xs={3} textAlign="center">
+                    <Typography><strong>INIT </strong>{char.initiative}</Typography>
+                  </Grid>
+                </Grid>
+              ))}
+            </Box>
+          </TabPanel>
+          <TabPanel value={valueTabMobile} index={3}>
+            <Box sx={{ width: '60vw' }}>
+
+            </Box>
+          </TabPanel>
+        </Box>
       </Box>
     </Container>
+
   );
 }
 
--- src/components/charList/CharList.tsx
+++ src/components/charList/CharList.tsx
@@ -1,5 +1,5 @@
 import { Add, Delete, Person, SmartToy } from '@mui/icons-material';
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
 import { Box, Container } from '@mui/system';
 import { useEffect, useState } from 'react';
 import { Character } from '../../types/Character';
@@ -18,18 +18,19 @@ export default function CharList(props: any) {
 
     function handle_TfInit(init: string, index: number) {
         listCharacter[index].initiative = init;
+        return listCharacter;
     }
     //#endregion
 
     //#region Click_Events()
     function click_AddBtn() {
 
-        listCharacter.push({
+        setListCharacter([...listCharacter, {
             name: charName,
             initiative: '',
             id: listCharacter.length + 1,
             flag: charType
-        })
+        }]);
         setCharName('');
         return listCharacter;
     }
@@ -48,43 +49,50 @@ export default function CharList(props: any) {
         };
 
         configure();
+        
     }, [props]);
+
+    useEffect(() => {
+        props.updateCharList(listCharacter);
+    }, [listCharacter]);
 
     return (
         <Container>
-            {/* Lista de personagens */}
-            {listCharacter.map((char, index) => (
-                <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }} key={index}>
-                    <Box sx={{ p: 2 }}>
-                        {props.render === 1 ? <Person /> : <SmartToy />}
-                    </Box>
-                    <Box sx={{ p: 2, minWidth: '175px' }}>
-                        <Typography>{char.name}</Typography>
-                    </Box>
-                    <Box sx={{ p: 2 }}>
-                        <TextField id="tfInit" variant="outlined" inputProps={{ maxLength: 2 }}
-                            sx={{ width: '65px', textAlign: 'center' }} onChange={e => handle_TfInit(e.target.value, index)} />
-                    </Box>
-                    <Box sx={{ p: 2 }}>
-                        <Button variant="outlined" color="error" sx={{ height: '55px' }}
-                            onClick={() => {props.updateCharList(click_DelBtn(char.id))}}>
-                            < Delete />
-                        </Button>
-                    </Box>
-                </Box>
-            ))}
+            <Box sx={{ display: 'inline' }}>
+                {/* Lista de personagens */}
+                {listCharacter.map((char, index) => (
+                    <Grid container spacing={1} alignItems="center" mb={3} >
+                        <Grid item xs={2}>
+                            {props.render === 1 ? <Person /> : <SmartToy />}
+                        </Grid>
+                        <Grid item xs={4} lg={6}>
+                            <Typography noWrap>{char.name}</Typography>
+                        </Grid>
+                        <Grid item xs={3} lg={2}>
+                            <TextField id="tfInit" variant="outlined" inputProps={{ maxLength: 2, style: { textAlign: 'center' } }}
+                                sx={{ textAlign: 'center'}} onChange={e => props.updateCharList(handle_TfInit(e.target.value, index))} />
+                        </Grid>
+                        <Grid item xs={3} lg={2}>
+                            <Button variant="outlined" color="error" sx={{ height: '55px' }}
+                                onClick={() => { props.updateCharList(click_DelBtn(char.id)); }}>
+                                <Delete />
+                            </Button>
+                        </Grid>
+                    </Grid>
+                ))}
 
-            {/* Input de personagem */}
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <Box sx={{ p: 2 }}>
-                    <TextField id="tfCharacter" label="Nome" variant="standard" value={charName}
-                        sx={{ width: '150px', textAlign: 'center' }} onChange={e => handle_TfName(e.target.value)} />
-                </Box>
-                <Box sx={{ p: 2 }}>
-                    <Button variant="contained" sx={{ height: '55px' }} onClick={() => {props.updateCharList(click_AddBtn())}}>
-                        <Add />
-                    </Button>
-                </Box>
+                {/* Input de personagem */}
+                <Grid container spacing={1} alignItems="center" mb={3} >
+                    <Grid item xs={8} >
+                        <TextField id="tfCharacter" label="Nome" variant="standard" value={charName}
+                            sx={{ textAlign: 'center' }} onChange={e => handle_TfName(e.target.value)} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained" sx={{ height: '55px' }} onClick={() => { props.updateCharList(click_AddBtn()) }}>
+                            <Add />
+                        </Button>
+                    </Grid>
+                </Grid>
             </Box>
         </Container>
     )
